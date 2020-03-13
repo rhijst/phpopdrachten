da76bdc759cc3f3751c9ff07943a6176beaa5e64
Thijs Teerhuis <thijs.teerhuis@edu-kw1c.nl>
2020-03-13 09:39:51 +0100

    4.3 af gemaakt

--- hoofdstuk4/Script.php
+++ hoofdstuk4/Script.php
@@ -21,11 +21,33 @@ for ($teller = 19; $teller >= 0; $teller--)
 
 // taak 3
 $task4 = "";
-$counter = 19;
-while ($counter >= 0)
+$counter2 = 1;
+while ($counter2 <= 6)
 {
-    $text = $text . $counter . ",";
-    $counter--;
+    $task4 .= "<h" . $counter2 .  ">Dit is de " . $counter2 . "e iteratie<br></" . $counter2 . ">";
+    $counter2++;
 }
 
+// taak 5
+$text3 ="";
+$task5 ="<table><tr>";
+for ($teller2 = 0; $teller2 <= 10; $teller2++)
+{
+    $task5 .= "<td>Dit is de" . $teller2 . "e iteratie</td>";
+}
+$task5 .= "</tr></table>";
+
+// taak 6
+$task6 = "";
+$counter3 = 2020;
+$extracounter=0;
+$extracounter1=2020;
+$extracounter2=2002;
+
+while ($counter3 >= 2002) {
+    $task6 .= "<p>In " . $counter3 . "was ik " . ($extracounter1-$extracounter2);
+    $counter3--;
+    $extracounter2++;
+    $extracounter++;
+}
 
--- hoofdstuk4/opdracht_4-3.php
+++ hoofdstuk4/opdracht_4-3.php
@@ -7,10 +7,15 @@ include "Script.php"
     <h2>Uitwerkingen</h2>
     <br><br><br>
     <?php
-    echo $task2;
-    //echo $text;
-    echo rtrim($text, ",");
-    echo rtrim($text2, ",");
+    echo "<h4>taak 2</h4><br>";
+    echo "<h4>taak 3</h4>";
+    echo  $task2 . rtrim($text, ", ") . "<br>";
+    echo "<h4>taak 4</h4>";
+    echo $task4;
+    echo "<h4>taak 5</h4>";
+    echo $task5;
+    echo "<h4>taak 6</h4>";
+    echo $task6;
     ?>
 </main>
 <?php
