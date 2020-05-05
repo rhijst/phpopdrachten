63bdce4ce19bfe595431542aac885628e6d72af2
Thijs Teerhuis <thijs.teerhuis@edu-kw1c.nl>
2020-05-05 20:07:14 +0200

    6.2 ietsje uitgebruider gemaakt

--- Hoofdstuk6/opdracht6.2.php
+++ Hoofdstuk6/opdracht6.2.php
@@ -2,10 +2,14 @@
 include "../include/header.php";
 include "../include/aside.php";
 ?>
-<main id="wrapper">
+<main id="wrapper" >
     <h2>
         hoofdstuk 6.1
     </h2>
+<!--    /////////////////////////////-->
+<!--    benodigde code-->
+<!--    onchange="document.getElementById('gameFrm').submit();-->
+<!--    heo moet ik plaatjes toevoegen? (toegang geweigerd geen machtigingen)-->
     <form id="gameFrm" method="get" action="opdracht62.php">
         <div class="float"><input type="radio"
                                   onchange="document.getElementById('gameFrm').submit();" name="keuze"
@@ -18,6 +22,38 @@ include "../include/aside.php";
                                   value="schaar"><img src="../images/schaar.jpg"></div>
     </form>
 </main>
+
+<?php
+    if (isset($_GET['keuze']))
+    {
+        echo "Jij koos: <img src='../images/{$_GET['keuze']}.jpg'>";
+        //keuze computer?
+    }
+
+
+    //Wat kiest de computer
+    //Random wordt er een getal tussen 0 en 2 gekozen
+//    $keuzecomputer = rand(0,2); //nodig of niet?
+    $opties = array("steen","papier","schaar");
+    $computerkeuzegetal = rand(0,2);
+    $computerkeuze = $opties[$computerkeuzegetal];
+    echo "&nbsp;&nbsp;De computer koos: <img src='../images/{$computerkeuze}.jpg'>";
+
+
+/*
+ *spelregels
+    * gelijke keuzes wint niemand
+    * schaar wint van papier
+    * schaar verliest van steen
+    * steen wint van schaar
+    * steen verliest van papier
+    * papier wint van steen steen
+    * papier verliest schaar
+*/
+
+?>
+
+
 <?php
 include "../include/footer.php"
 ?>
